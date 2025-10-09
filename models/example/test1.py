317b24971681278ae08dcd95e2ce201dfed55caf
Jialuo Chen <jialuo@google.com>
2025-10-09 22:12:43 +0000

    add creds.refresh(Request())

--- models/example/test1.py
+++ models/example/test1.py
@@ -1,9 +1,7 @@
 def model(dbt, session):
     dbt.config(
         submission_method="bigframes",
-        notebook_template_id='8573503490234515456',
+        notebook_template_id='8573503490234515456',     # If I remove this line, it works fine
     )
-
     data = {'id': [0, 1, 3],'name': ['Brian', 'Isaac', 'Marie']}
-    print('** test1: notebook_template_id=8573503490234515456 **')
     return bpd.DataFrame(data)
